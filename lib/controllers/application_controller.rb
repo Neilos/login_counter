0327e475db22ec2f257fe79d7d3cc842093620c2
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-07-05 11:13:56 +0100

    fixed root directory erro

--- lib/controllers/application_controller.rb
+++ lib/controllers/application_controller.rb
@@ -2,6 +2,14 @@ require 'sinatra/base'
 
 class ApplicationController < Sinatra::Base
 
+# configure sinatra app
+configure do
+  set :root, Proc.new { File.join(File.dirname(__FILE__), "../../") }
+  use Rack::Session::Cookie, 
+      :key => 'rack.session', 
+      :secret => 'how many times are you going to login'
+end
+
 get '/?' do
   erb :home
 end
