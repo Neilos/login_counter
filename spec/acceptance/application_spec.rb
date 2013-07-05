a31e9b05c34f27116e032ec78ddcffd57a80667c
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-07-05 13:55:21 +0100

    plain text acceptance tests written

--- spec/acceptance/application_spec.rb
+++ spec/acceptance/application_spec.rb
@@ -21,13 +21,63 @@ end
 # Set the application to be used by capybara
 Capybara.app = ApplicationController
 
-describe "the application", :type => :feature do
+
+feature "visiting the home page" do
+
+  scenario "when logged out"
+    # it should explain what the app does
+    # it should have a login form
+    # it should have a signup form
+
+  scenario "when logged in"
+    # it should show the login count page
+
+end
+
+feature "signing up" do
   
-  describe "when...", :type => :feature  do
+  scenario "with invalid email"
+    # it should display an appropriate error message
+    # it wont sign up the user
 
-    it "should..."
+  scenario "with email already used"
+    # it should display an appropriate error message
+    # it wont sign up the user
 
-  end
+  scenario "when password doesn't match password confirmation"
+    # it should display an appropriate error message
+    # it wont sign up the user
+
+  scenario "when password is not of the correct length"
+    # it should display an appropriate error message
+    # it wont sign up the user
+
+  scenario "valid details are entered"
+    # it should add the user to the database
+    # it should confirm the signup
+    # it wont show the signup form
+
+end
+
+feature "logging in" do
+  
+  scenario "with invalid email"
+    # it should display an appropriate error message
+    # it wont log the user in
+
+  scenario "with wrong password"
+    # it should display an appropriate error message
+    # it wont log the user in
+
+  scenario "with correct details"
+    # it should show the login count page
+
+end
+
+feature "logging out" do
+
+  scenario "when logged in"
+    # it should show the home page
 
 end
 
